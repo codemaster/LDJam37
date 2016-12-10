2bd14dc143e15a4e89cac4405dd5e53ac3272019
Andrew Kane <codemaster@quantumraiders.com>
2016-12-10 09:33:50 -0800

    Add initial character controller.

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Sparrow
+{
+	/// <summary>
+	/// Controller for the character
+	/// </summary>
+	public class CharacterController : MonoBehaviour
+	{
+		/// <summary>
+		/// The movement speed of the character
+		/// </summary>
+		public float MovementSpeed;
+
+		/// <summary>
+		/// Updates once per frame
+		/// </summary>
+		void Update()
+		{
+			var horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * MovementSpeed;
+			var vertical = Input.GetAxis("Vertical") * Time.deltaTime * MovementSpeed;
+			transform.Translate(horizontal, vertical, 0f);
+		}
+
+		/// <summary>
+		/// Validates variables in the Inspector
+		/// </summary>
+		void OnValidate()
+		{
+			if (MovementSpeed <= 0f)
+			{
+				MovementSpeed = 1f;
+			}
+		}
+	}
+}
