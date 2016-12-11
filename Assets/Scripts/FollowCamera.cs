5eb53b1723fb3e3055cfd3b6af322d1dfb22c345
Andrew Kane <codemaster@quantumraiders.com>
2016-12-10 19:23:44 -0800

    Added Follow-Camera script

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Sparrow
+{
+	/// <summary>
+	/// Script to have the camera follow the character
+	/// </summary>
+	public class FollowCamera : MonoBehaviour
+	{
+		/// <summary>
+		/// Triggers after the frame update
+		/// </summary>
+		void LateUpdate()
+		{
+			var character = CharacterController.Instance;
+			if (null != character)
+			{
+				var characterPosition = character.transform.position;
+				transform.position = new Vector3(characterPosition.x, characterPosition.y, transform.position.z);
+			}
+		}
+	}
+}
