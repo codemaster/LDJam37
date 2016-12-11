5486c9c225d77df9e47a5f9e7487f79b5490066c
Christophe-X99\Christophe <cs@cesium-audio.com>
2016-12-11 22:36:18 +0100

    Added the music system and some script for testing the parameters.

---
+++ Assets/Scripts/MusicTester.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTester : MonoBehaviour {
+
+	public MusicControl musicSystem;
+
+	public float inGame = 0.0f;
+	public float stress = 0.0f;
+	public float speed = 0.0f;
+	public float creep = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		musicSystem.InGameMusic (inGame);
+		musicSystem.StressLevel (stress);
+		musicSystem.SpeedLevel (speed);
+		musicSystem.CreepValue (creep);
+		
+	}
+}
